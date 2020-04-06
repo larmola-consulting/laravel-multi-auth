2b14f03bd809de517782b07445e55174a7ffecfb
Kgaugelo Bopape <kgaugelo20@gmail.com>
2020-04-06 22:37:28 +0200

    Initial commit

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\LoginRequest;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class AdminController extends Controller
+{
+    use AuthenticatesUsers;
+
+    protected $guardName = 'admin';
+    protected $maxAttempts = 3;
+    protected $decayMinutes = 2;
+
+    protected $loginRoute;
+
+    public function __construct()
+    {
+        $this->middleware('guest:admin')->except('postLogout');
+        $this->loginRoute = route('admin.login');
+    }
+
+    public function getLogin()
+    {
+        return view('admin.login');
+    }
+
+    public function postLogout()
+    {
+        Auth::guard($this->guardName)->logout();
+        Session::flush();
+        return redirect()->guest($this->loginRoute);
+    }
+
+    public function postLogin(LoginRequest $request)
+    {
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+            $this->sendLockoutResponse($request);
+        }
+
+        $credential = [
+            'email' => $request->input('email'),
+            'password' => $request->input('password')
+        ];
+
+        if (Auth::guard($this->guardName)->attempt($credential)) {
+            $request->session()->regenerate();
+            $this->clearLoginAttempts($request);
+            return redirect()->intended('/dashboard');
+        } else {
+            $this->incrementLoginAttempts($request);
+
+            return redirect()->back()
+                ->withInput()
+                ->withErrors(["Incorrect user login details!"]);
+        }
+    }
+}
---
+++ app/Model/Admin.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Admin extends Authenticatable
+{
+    use Notifiable;
+
+    protected $guard = 'admin';
+
+    protected $fillable = ['name', 'email', 'username', 'password','email_verfied_at'];
+
+    protected $hidden = ['password'];
+}
